ece54afc08a88a52c5b70c5e7983662972f4bec4
Henry Warhurst <hwarhurst@palantir.com>
2020-01-05 22:04:58 +1100

    Update pre-commit code

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include <iostream>
 
 int main() {
-  std::cout <<
-      "Hello, Matrix"
-                                           << std::endl;
+  std::cout << "Hello, Matrix" << std::endl;
 
-  return                                                   0;
+  return 0;
 }
